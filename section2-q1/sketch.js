7154e684e19dcabb39d41e44f3c7b27770b9e442
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-22 06:30:53 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,13 +2,5 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
-  createCanvas(100, 100)
-  ellipse(50, 50, i)
-  if(0 <= i <= 5){
-    stroke(0, 0, 255);
-    }
-  else(5 < i <= 10){
-    stroke(255, 0, 0);
-    }
   }
 }
