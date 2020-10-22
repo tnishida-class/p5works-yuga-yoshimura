25c6211b4493822db1cbc814af0a5a5a905e59e5
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-23 00:16:37 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-      if(i % 2 = 1){
+      if(i % 2 == 1){
         rect(i * 20, j * 20 ,25 ,25);
         fill(120)
       }
