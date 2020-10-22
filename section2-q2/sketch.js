d4a31a6a469e72fcce4ad92bbc71c4e084e2af60
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-23 01:34:46 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -15,7 +15,7 @@ function setup() {
       rect(0, 25, 25, 25)
 
       if(background != 196){
-        fill(255, 0, 0)
+        color(255, 0, 0)
         ellipse((i - 0.5) * 25, (j - 0.5) * 25, 20);
       }
         else{
