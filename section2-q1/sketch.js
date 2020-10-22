f1c93ab0dfe33adde4eef26d3d652b3cc9109830
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-22 23:34:50 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,9 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 50; i > 0; i = i - 10){
+  for(let i = 100; i > 0; i = i - 10){
     // BLANK[1]
     noFill();
-    stroke(0, 0, 255);
+    if(i < 50){
+      stroke(0, 0, 255);
+    }
+    else{
+      stroke(255, 0, 0);
+    }
     ellipse(50, 50, i);
   }
 }
