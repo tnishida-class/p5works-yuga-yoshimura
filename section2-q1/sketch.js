a420e918590965c7c4815779466ad8f56a4f821d
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-22 23:14:12 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -3,7 +3,7 @@ function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
     nofill();
-    ellipse(50, 50, i)
-    stroke(0, 0, 255)
+    ellipse(50, 50, i);
+    stroke(0, 0, 255);
   }
 }
