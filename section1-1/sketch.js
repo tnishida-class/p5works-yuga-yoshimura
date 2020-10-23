759dc3ee37a21876146b3e7f2eba78e054dc9d49
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-24 04:10:35 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,16 +1,33 @@
 function setup() {
-  createCanvas(128,128);
-}
-
-function draw() {
-  background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
-  fill(255);
-  textSize(32);
-  textFont("serif");
-  text("46", 68, 100);
+  createCanvas(200, 200);
+  let size = width / 8;
+  noStroke();
+  for(let i = 0; i < 8; i++){
+    for(let j = 0; j < 8; j++){
+      fill(196);
+      if(j%2==0){
+        rect(50 * i + 25, 25 * j, 25, 25);
+        if(j<3){
+        fill(255, 0, 0);
+        ellipse(37.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+      }
+        else if(j>4){
+          fill(0);
+          ellipse(37.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+        }
+      }
+      else{
+        rect(50 * i, 25 * j, 25, 25);
+        if(j<3){
+        fill(255, 0, 0);
+        ellipse(12.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+      }
+        else if(j>4){
+          fill(0);
+          ellipse(12.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+        }
+     }
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    }
+  }
 }
