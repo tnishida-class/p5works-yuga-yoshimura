b8df71e9df33782e57826545b955f2cc1b123020
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-24 04:13:52 +0900

    Update sketch.js

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,33 +1,21 @@
+// チェッカー
 function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      fill(196);
-      if(j%2==0){
-        rect(50 * i + 25, 25 * j, 25, 25);
-        if(j<3){
-        fill(255, 0, 0);
-        ellipse(37.5 + 50 * i, 12.5 + 25 * j, 20, 20);
-      }
-        else if(j>4){
-          fill(0);
-          ellipse(37.5 + 50 * i, 12.5 + 25 * j, 20, 20);
-        }
-      }
-      else{
-        rect(50 * i, 25 * j, 25, 25);
-        if(j<3){
-        fill(255, 0, 0);
-        ellipse(12.5 + 50 * i, 12.5 + 25 * j, 20, 20);
-      }
-        else if(j>4){
-          fill(0);
-          ellipse(12.5 + 50 * i, 12.5 + 25 * j, 20, 20);
-        }
-     }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      let k = i + j
+      if(k % 2 == 1){
+        rect(size*i, size*j, size, size);
+        fill(120);
+      }
+      if(k % 2 == 0 && i != j){
+        fill(255, 0, 0)
+        ellipse(size * (i - 0.5), size * (j - 0.5), 20)
+      }
+      else{}
+  }
   }
 }
