1101e71992799b1220f2b46934eef44730b09061
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-10-24 04:58:45 +0900

    Update sketch.js

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -18,7 +18,7 @@ function setup() {
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
-  
+  drawCircle(red, maxR * 0.05);
 }
 
 function drawCircle(c, r){
