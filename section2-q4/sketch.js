759c996cd771e220f0f437c0d90f12f32a9f0fcb
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-07 20:17:21 +0900

    Update sketch.js

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -10,10 +10,7 @@ function setup() {
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d)
-    if(i % 2 == 0){
-      fill('white');
-    }
-    else{
+    if(i % 2 == 1|| i == 0){
       fill(blue);
     }
 }
