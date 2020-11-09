1e29f7244927a39ae223c166a11587e697db21ea
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-10 02:36:57 +0900

    Update sketch.js

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -2,11 +2,11 @@
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background('blue');
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    star(x, y, 10);
+    ellipse(x, y, 10);
   }
 }
