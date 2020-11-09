af3d68616c1141fb29e645600bee8c0b23f67342
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-10 02:53:51 +0900

    Update sketch.js

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -3,10 +3,13 @@
 function setup(){
   createCanvas(300, 200);
   background('blue');
+  star(x, y, r)
+}
+
+function star(x, y, r){
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
     let x = 150 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
   }
 }
