76504decb8a2979e42a7b034c2ae721e212965f9
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-10 03:06:55 +0900

    Update sketch.js

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -7,9 +7,7 @@ function setup(){
   star(150, 100, 50);
 }
 
-function setup(){
-  createCanvas(200, 200);
-  background(192);
+function star(cx, cy, r){
   for(let i = 0; i < 12; i++){
     const theta = TWO_PI * i / 12; // TWO_PI は円周率πの2倍（ほかに PI, HALF_PI, QUARTER_PI　がある）
     const x = 100 + cos(theta) * 50; // 関数 cos の戻り値を使用
