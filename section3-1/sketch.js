c91ca6a9b05d69bf81a1bf89f2360981bc3f7646
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-10 02:37:32 +0900

    Update sketch.js

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -1,7 +1,7 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(300, 200);
   background('blue');
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
