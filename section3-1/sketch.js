dde84d38a2a6f068f4b17640a170c74b69468536
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-14 18:17:11 +0900

    Update sketch.js

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -8,10 +8,13 @@ function setup(){
 }
 
 function star(cx, cy, r){
-  for(let i = 0; i < 12; i++){
-    const theta = TWO_PI * i / 12;
+  push();
+  beginshape();
+  for(var i = 0; i < 12; i++){
+    const theta = TWO_PI * i * 2 / 5 - HALF_PI;
     const x = 100 + cos(theta) * 50;
     const y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    vertex(x, y);
   }
+  endshape();
 }
