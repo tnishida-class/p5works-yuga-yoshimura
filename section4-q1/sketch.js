5c5719988b4e996b63143a8ab1a955ef1581e91d
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-23 05:17:31 +0900

    Update sketch.js

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -11,7 +11,6 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1);
-  let size = count 
-  constrain(size, count, cycle);
+  size = constrain(size, count, cycle);
   ellipse(width / 2, height / 2, size);
 }
