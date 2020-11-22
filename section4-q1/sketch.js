cf97ecaa1e97b9855d9eaf0abd8af6f44d937324
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-23 04:58:51 +0900

    Update sketch.js

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -4,12 +4,13 @@ let cycle;
 
 function setup(){
   createCanvas(200, 200);
-  count = 0;
+  count = 50;
   cycle = 100;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
+
   ellipse(width / 2, height / 2, count);
 }
