994313b4bb27674c0557f50ac22381634f5f87c8
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-24 04:03:34 +0900

    Update sketch.js

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -11,3 +11,28 @@ function draw(){
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+let count;
+let cycle;
+
+function setup(){
+  createCanvas(200, 200);
+  count = 0;
+  cycle = 60;
+}
+
+function draw(){
+  background(160, 192, 255);
+  count = (count + 1) % cycle;
+  // BLANK[1]
+  let size;
+  size = count + 50;
+  // size += 1;
+  fill(199,60,46);
+  ellipse(width / 2, height / 2, size);
+  // size += 100;
+
+  if (size > 160) {
+    // size += 50;
+    size = 150;
+  }
+}
