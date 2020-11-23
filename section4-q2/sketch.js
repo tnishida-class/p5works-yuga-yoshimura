574b68c435ffc9a7456c9c619036f09bfd33fa74
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-23 15:16:09 +0900

    Update sketch.js

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -12,7 +12,7 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
-  star(x, y, r, angle)
+  star(x, y, width / 2, angle)
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
