064dd82ea855b43eb7237fd6c974886d5bd26286
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-24 04:22:55 +0900

    Update sketch.js

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,11 +1,48 @@
 // 最終課題を制作しよう
+let x, y, vx, vy;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  x = width / 2;
+  y = height / 2;
+  vx = 8;
+  vy = 8;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
+  // BLANK[2] (hint: 作った star 関数を使います)
+  star(x, y, 50, angle);
+  angle += 10;
+  x += vx;
+  y += vy;
+  if(keyIsDown(LEFT_ARROW)){vx = vx + 5};
+  if(keyIsDown(RIGHT_ARROW)){vx = vx - 5};
+  if(keyIsDown(UP_ARROW)){vy = vy + 5};
+  if(keyIsDown(DOWN_ARROW)){vy = vy - 5};
+  // 端の処理パターン (1) 反対側から出てくる
+  if(x > width){ x = 0; }
+  else if(x < 0){ x = width; }
+  if(y > height){ y = 0; }
+  if(y < 0){ y = height; }
+}
+
+function star(cx, cy, r, angle){
+  beginShape();
+  for(var i = 0; i < 20; i++){
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
+    // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+    var x = cx + cos(theta) * r;
+    var y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+}
+
+function mousePressed(){
+  if(mousePressed){x = mouseX ; y = mouseY}
+  else{x = x ; y = y}
 }
 
 function windowResized(){
