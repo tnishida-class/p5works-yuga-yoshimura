4353648cffab5ef6282edbbdd4137335a89ea8ac
yuga-yoshimura <72771519+yuga-yoshimura@users.noreply.github.com>
2020-11-24 10:12:49 +0900

    Update sketch.js

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -11,24 +11,6 @@ function setup(){
   z = 0
 }
 
-let inconsolata;
-function preload() {
-  inconsolata = loadFont('assets/inconsolata.otf');
-}
-function setup() {
-  createCanvas(100, 100, WEBGL);
-  textFont(inconsolata);
-  textSize(width / 3);
-  textAlign(CENTER, CENTER);
-}
-function draw() {
-  background(0);
-  let time = millis();
-  rotateX(time / 1000);
-  rotateZ(time / 1234);
-  text('p5.js', 0, 0);
-}
-
 function draw(){
   background(160, 192, 255);
   textSize(width / 20)
